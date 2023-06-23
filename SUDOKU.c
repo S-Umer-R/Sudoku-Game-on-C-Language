48a079f79ad2ce4dcd64e5be2aa65514993656e7
S-Umer-R <115623323+S-Umer-R@users.noreply.github.com>
2023-06-24 01:44:47 +0500

    Update SUDOKU.c

--- SUDOKU.c
+++ SUDOKU.c
@@ -1,497 +1,289 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+#include <string.h>
+#include <time.h>
 
-#define SIZE 4
-#define SOZE 9
+#define SIZE 9
+#define SOZE 3
 
-void inputBoard(int [][SIZE]);                             /*SUDOKU SOLVER PROTOTYPE*/
-void outputBoard(int [][SIZE]);                            /*SUDOKU SOLVER PROTOTYPE*/
-void solvingSudoku(int [][SIZE]);                          /*SUDOKU SOLVER PROTOTYPE*/
-int nextround(int [][SIZE]);                               /*SUDOKU SOLVER PROTOTYPE*/
-void check(int [][SIZE], int,int,int,int);                 /*SUDOKU SOLVER PROTOTYPE*/
-void sudoku();                                             /*SUDOKU MAIN GAME PROTOTYPE*/
-void display();                                            /*SUDOKU HIGHSCORES PROTOTYPE*/
-int SudokuChecker(int sud[4][4]);                          /*SUDOKU CHECKER PROTOTYPE*/
-
-struct score{
-	char player[20];
-	int lives;
-	char print[5];
+struct score {
+    char player[20];
+    int lives;
 };
-int main(void){
-	int request, i, j;
-	
-	printf("%s","\n|| SUDOKU ||\n""\n1  Play game""\n2  Sudoku Solver""\n3  Highscore""\n0  Exit""\nEnter your choice: ");
-	scanf("%d", &request);
-	system("cls");
-	while(request!=0){
-		
-		switch(request){
-			case 1:{
-			sudoku();
-			break;
-			}
-			case 2:{
-				int brd[SIZE][SIZE];
-				
-				printf("Enter the sudoku pattern.Enter 0 for any blank cells\n");
-            
-                inputBoard(brd);
-                solvingSudoku(brd);
-                printf("Here is the solved Sudoku puzzle!\n");
-                outputBoard(brd);
-                system("pause");
-                system("cls");
-				
-				break;
-			}
-			case 3:{
-				display();puts("");system("pause");system("cls");
-				break;
-			}
-			case 0:{
-				break;
-				break;
-			}
-			default:{
-				printf("\nInvalid input\n");
-				break;
-			}
-		}
-		printf("%s","\n|| SUDOKU ||\n""\n1  Play game""\n2  Sudoku Solver""\n3  Highscore""\n0  Exit""\nEnter your choice: ");
-		scanf("%d", &request);
-		system("cls");
-	}
-}
 
-void display(){/*SUDOKU HIGHSCORES*/
-struct score p1;
-FILE *fdisplay;
-fdisplay=fopen("Scores.DAT","r");
+void printBoard(int grid[][SIZE]);
+void playGame(int level);
+int isValidMove(int grid[][SIZE], int row, int col, int num);
+int isBoardFull(int grid[][SIZE]);
+int solveSudoku(int grid[][SIZE]);
+int nextround(int grid[][SIZE]);
+void check(int grid[][SIZE], int row, int col);
+void sudoku(int grid[][SIZE]);
+void saveScore(struct score player);
+void displayHighscores();
+void solveSudokuPuzzle(int grid[][SIZE]);
+void copyGrid(int grid[][SIZE], int copyGrid[][SIZE]);
 
-if(fdisplay==NULL){
-	printf("\nError, file does not exist!!");
-	exit(1);}
-	
-while(fgets(p1.print,5,fdisplay)!=NULL){
-	printf("%s", p1.print);
-}
-fclose(fdisplay);
-}
-
-          void inputBoard(int brd[][SIZE])/*SUDOKU SOLVER*/
-          
-{
-    int m,n;
-    for (m=0;m<SIZE;m++)
-   {
-    for(n=0;n<SIZE;n++)
-    scanf("%d", &brd[m][n]);
-   }
-}
-
-
-
-          void outputBoard(int brd[][SIZE])/*SUDOKU SOLVER*/
-{
-	
-    int m,n;
-    for (m=0;m<SIZE;m++)
-   {
-    for (n=0;n<SIZE;n++)
-    printf("%d ",brd[m][n]);
-    printf("\n");
-   }
-}
-
-
-
-
-          void solvingSudoku(int brd[][SIZE])/*SUDOKU SOLVER*/
-{
-	
-    int emptycellLocated;
-    do
-   {
-    emptycellLocated = nextround(brd);
-   }
-   while(emptycellLocated);
-   }
-
-
-   int nextround(int brd[][SIZE])/*SUDOKU SOLVER*/
-   
-{
-   int m,n;
-   int sum; 
-   int zerocounter;
-
- 
-   for(m=0;m<SIZE;m++)
-  {
-   sum=0;
-   zerocounter=0;
-
-   for(n=0;n<SIZE;n++)
-  {
-     if(brd[m][n]==0)
-     zerocounter++;
-     else
-     {
-     sum = sum + brd[m][n];
-     }
-  }
-     if(zerocounter == 1)
-     { 
-    for (n=0;n<SIZE;n++) 
-   {
-     if (brd[m][n]==0) 
-	 {
-    brd[m][n]=10-sum;
-    break;
-     }
-   }
-     }
-   }
-
-    for (n=0;n<SIZE;n++)
-   {
-    sum=0;
-    zerocounter=0;
-
-    for(m=0;m<SIZE;m++) 
-   {
-     if(brd[m][n]==0)
-     {
-    zerocounter++;
-	 }
-     else
-    sum=sum + brd[m][n];
-   }
-     if(zerocounter==1) 
-	 {  
-   for(m=0;m<SIZE;m++)
-  {
-     if (brd[m][n]==0)
-	 {
-   brd[m][n]=10-sum;  
-   break;
-     }
-  }
-     }
-  }
- 
-   check(brd, 0, 2, 0, 2);
-   check(brd, 0, 2, 2, 4);
-   check(brd, 2, 4, 0, 2);
-   check(brd, 2, 4, 2, 4);
-
-
-   for(m=0;m<SIZE;m++)
-  {
-   for(n=0;n<SIZE;n++)
-  {
-    if(brd[m][n]==0)  
-   return 1;
-    }
- }
-   return 0;  
-}
-
-
-
-
-           void check(int brd[][SIZE],int lrow,int urow,int lcol,int ucol)
-{
-	
-   int m,n;
-   int total=0;  
-   int zerocounter=0; 
-
-   for (m=lrow;m<urow;m++)
-  {  
-   for (n=lcol;n<ucol;n++)
-  {
-    if(brd[m][n]==0)
-    {
-   zerocounter++;
-    }
-    else
-    {
-   total+=brd[m][n];
-    }
-  }
-  }
-    if(zerocounter==1) 
-	{  
-   for (m=lrow;m<urow;m++)
-  {
-   for(n=lcol;n<ucol;n++)
-  { 
-    if(brd[m][n]==0)
-    {
-   brd[m][n]=10-total;  
-   break;
-    }
-  }
-  }
-    }
-}
-
-int SudokuChecker(int sud[4][4])/*SUDOKU CHECKER*/
-{
-    int i,j,k,a,b,c,temp=1;
-        for(i=0;i<4;i++)
-        {
-          for(j=0;j<4;j++)
-          {
-            if(sud[i][j]==0)
-              temp=2;
-          }
+void printBoard(int grid[][SIZE]) {
+    int i, j;
+    printf("\n    Sudoku Board\n");
+    printf("  ---------------------\n");
+    for (i = 0; i < SIZE; i++) {
+        printf("  ");
+        for (j = 0; j < SIZE; j++) {
+            if (j % SOZE == 0 && j != 0) {
+                printf("| ");
+            }
+            if (grid[i][j] == 0) {
+                printf("- ");
+            } else {
+                printf("%d ", grid[i][j]);
+            }
         }
-        if(temp==1)
-        {
-    for(i=0;i<4;i++)
-    {
-        for(j=0;j<4;j++)
-        {
-            for(a=0;a<4;a++)
-            {
-                if(a!=i && sud[i][j]==sud[a][j])
-                    return 0;
-                if(a!=j && sud[i][j]==sud[i][a])
-                    return 0;
-                if(i<2 && j<2)
-                {
-                    for(b=0;b<2;b++)
-                    {
-                        for(c=0;c<2;c++)
-                        {
-                            if((b!=i || c!=j) && sud[i][j]==sud[b][c])
-                                return 0;
-                        }
-                    }
-                }
-                else if(i<2 && j<4)
-                {
-                    for(b=0;b<2;b++)
-                    {
-                        for(c=2;c<4;c++)
-                        {
-                            if((b!=i || c!=j) && sud[i][j]==sud[b][c])
-                                return 0;
-                        }
-                    }
-                }
-                else if(i<4 && j<2)
-                {
-                    for(b=2;b<4;b++)
-                    {
-                        for(c=0;c<2;c++)
-                        {
-                            if((b!=i || c!=j) && sud[i][j]==sud[b][c])
-                                return 0;
-                        }
-                    }
-                }
-                else if(i<4 && j<4)
-                {
-                    for(b=2;b<4;b++)
-                    {
-                        for(c=2;c<4;c++)
-                        {
-                            if((b!=i || c!=j) && sud[i][j]==sud[b][c])
-                                return 0;
-                        }
-                    }
-                }
+        printf("\n");
+        if ((i + 1) % SOZE == 0 && i != SIZE - 1) {
+            printf("  ------|-------|------\n");
+        }
+    }
+    printf("  ---------------------\n");
+}
+
+void playGame(int level) {
+    int grid[SIZE][SIZE];
+    int i, j;
+
+    // Initialize grid with zeros
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            grid[i][j] = 0;
+        }
+    }
+
+    // Assign random values to some cells based on the selected level
+    int filledCells;
+    if (level == 1) {
+        filledCells = 30;
+    } else if (level == 2) {
+        filledCells = 20;
+    } else if (level == 3) {
+        filledCells = 10;
+    }
+
+    srand(time(NULL));
+    int count = 0;
+    while (count < filledCells) {
+        int row = rand() % SIZE;
+        int col = rand() % SIZE;
+        int num = rand() % SIZE + 1;
+        if (grid[row][col] == 0 && isValidMove(grid, row, col, num)) {
+            grid[row][col] = num;
+            count++;
+        }
+    }
+
+    printBoard(grid);
+    sudoku(grid);
+}
+
+int isValidMove(int grid[][SIZE], int row, int col, int num) {
+    int i, j;
+    // Check if the number already exists in the same row or column
+    for (i = 0; i < SIZE; i++) {
+        if (grid[row][i] == num || grid[i][col] == num) {
+            return 0;
+        }
+    }
+
+    // Check if the number already exists in the same 3x3 box
+    int startRow = row - row % SOZE;
+    int startCol = col - col % SOZE;
+    for (i = 0; i < SOZE; i++) {
+        for (j = 0; j < SOZE; j++) {
+            if (grid[startRow + i][startCol + j] == num) {
+                return 0;
             }
         }
     }
-    	}
-}
-void sudoku(){/*SUDOKU MAIN GAME*/
-	int level, row, col, i, j, p, sud[SIZE][SIZE];
-	char sudoku[SOZE][SOZE], newvalue, *newptr=&newvalue, uplimit='4', downlimit='1', rand1='1', rand2='2', rand3='3', rand4='4', nin='-', player[20], enter[]="\n";
-	struct score p1;
-	p1.lives=3;
-	for(i=0;i<SOZE;i++){
-		for(j=0;j<SOZE;j++){
-			if(i==0||i==2||i==4||i==6||i==8){
-			sudoku[i][j]='_';
-			}
-			else if((i==1||i==3||i==5||i==7)&&(j==0||j==2||j==4||j==6||j==8)){
-			    sudoku[i][j]='|';
-			    }
-			else{
-				sudoku[i][j]='-';
-			    }
-			
-		    }
-	    }
-	printf("%s","\n1 - Level""\n2 - Level""\n3 - Level""\nWhich level do you prefer? ");scanf("%d", &level);
-	if(level==1){
-		sudoku[1][7]=rand3;
-		sudoku[3][3]=rand1;sudoku[3][5]=rand2;sudoku[3][7]=rand4;
-		sudoku[5][1]=rand4;sudoku[5][3]=rand2;sudoku[5][5]=rand3;sudoku[5][7]=rand1;
-		sudoku[7][1]=rand1;sudoku[7][3]=rand3;sudoku[7][5]=rand4;sudoku[7][7]=rand2;
-	    }
-	else if(level==2){
-	    sudoku[1][3]=rand2;sudoku[1][5]=rand4;
-		sudoku[3][1]=rand1;sudoku[3][5]=rand2;sudoku[3][7]=rand3;
-		sudoku[5][1]=rand4;sudoku[5][7]=rand2;
-		sudoku[7][3]=rand1;sudoku[7][5]=rand3;sudoku[7][7]=rand4;
-		}
-	else if(level==3){
-		sudoku[3][5]=rand1;
-		sudoku[5][1]=rand4;
-		sudoku[7][3]=rand2;sudoku[7][7]=rand3;
-	    }
-	
-	printf("\n\t\t\t  SUDOKU\n");
-	for(i=0;i<SOZE;i++){
-	    printf("\n\t\t\t");
-		for(j=0;j<SOZE;j++){
-			printf("%c", sudoku[i][j]);
-		    }
-	    }
-	puts("");
-	
-while(p1.lives!=0){
-	col = 0, row = 0;
-	printf("\nEnter the cursor row and column ( 1, 2, 3, 4 ): ");
-	printf("\nRow Number: "); scanf("%d",&row);
-	//For Row
-	if(row == 2)
-	{
-		row = 3;
-	}
-	
-	else if(row == 3)
-	{
-		row = 5;
-	}
-	
-	else if(row == 4)
-	{
-		row = 7;
-	}
-	printf("Column Number: "); scanf("%d",&col);
-	//For Column
-	if(col == 2)
-	{
-		col = 3;
-	}
-	
-	else if(col == 3)
-	{
-		col = 5;
-	}
-	
-	else if(col == 4)
-	{
-		col = 7;
-	}
-	
-	if( (row==3 && col==5)|| (row==5 && col==1) || (row==7 && col==3) || (row==7 && col==7) ||(row==0||row==2||row==4||row==6||row==8) ||
-		((row==1||row==3||row==5||row==7)&&(col==0||col==2||col==4||col==6||col==8)) || (row>9||col>9)
-	    || (level==1 && ( (row==1 && col==7) || (row==3 && col==3) || (row==3 && col==7)
-		|| (row==5 && col==1) || (row==5 && col==3) || (row==5 && col==5) || (row==5 && col==7)
-		|| (row==7 && col==1) || (row==7 && col==3) || (row==7 && col==5) || (row==7 && col==7) ) )
-		            
-		|| (level==2 && ( (row==1 && col==3) || (row==1 && col==5) || (row==3 && col==1)
-		|| (row==3 && col==7) || (row==5 && col==1) || (row==5 && col==7) || (row==7 && col==3)
-		|| (row==7 && col==5) ) )
-					
-		|| (level==3 && ( (row==3 && col==5) || (row==5 && col==1)
-		|| (row==7 && col==3) || (row==7 && col==7) ) ) ){
-			 	
-		printf("\nWrong Input for row, and column!!  ");
-		puts("");
-		system("pause");
-		system("cls");
-		
-		
-        }
-	else{
-		printf("\nEnter value ( 1, 2, 3, 4 ): ");scanf(" %c", newptr);
-		if(*newptr>uplimit || *newptr<downlimit){
-		printf("\nWrong input for value!!  ");
-		puts("");
-		system("pause");
-		system("cls");
-		}
-		else{
-		sudoku[row][col]=*newptr;
-		system("cls");
-		}
-		
-	    }
-    printf("\n\t\t\t  SUDOKU\n");
-	for(i=0;i<SOZE;i++){
-		printf("\n\t\t\t");
-		for(j=0;j<SOZE;j++){
-			printf("%c", sudoku[i][j]);
-		    }
-	    }
-	puts("");
-	if((level==1&&( (sudoku[1][1]!='-') && (sudoku[1][3]!='-') && (sudoku[1][5]!='-') && (sudoku[3][1]!='-') && (sudoku[3][5]!='-')))
-				 
-	    || (level==2 && ( (sudoku[1][1]!='-') && (sudoku[1][7]!='-') && (sudoku[3][3]!='-') && (sudoku[3][5]!='-') && (sudoku[5][3]!='-')
-		&& (sudoku[5][5]!='-') && (sudoku[7][1]!='-') && (sudoku[7][7]!='-') ))
-				
-		|| (level==3 && ( (sudoku[1][1]!='-') && (sudoku[1][3]!='-') && (sudoku[1][5]!='-') && (sudoku[1][7]!='-') && (sudoku[3][1]!='-') && 
-		(sudoku[3][3]!='-') && (sudoku[3][7]!='-') && (sudoku[5][3]!='-') && (sudoku[5][5]!='-') && (sudoku[5][7]!='-') && (sudoku[7][1]!='-') && (sudoku[7][5]!='-')))){
-				  	
-				  		
-		for(p=0,j=1;j<SOZE;p++,j+=2){
-			sud[0][p]=sudoku[1][j]-48;
-			}
-						
-		for(p=0,j=1;j<SOZE;p++,j+=2){
-			sud[1][p]=sudoku[3][j]-48;
-			}
-		for(p=0,j=1;j<SOZE;p++,j+=2){
-		    sud[2][p]=sudoku[5][j]-48;
-			}
-		for(p=0,j=1;j<SOZE;p++,j+=2){
-			sud[3][p]=sudoku[7][j]-48;
-			}
-					
-		int solution=(SudokuChecker(sud)), i, j;
-                            
-		if(solution==0){
-            printf("\n\t\tThe solution is Incorrect!");
-			p1.lives--;
-			}
-		else{
-		    printf("\n\n\t\t\tYou Won!!");
-			FILE *fscore;
-            fscore=fopen("Scores.DAT","a+");
 
-            if(fscore==NULL){
-	        printf("\nError, file does not exist!!");
-			exit(1);
+    return 1;
+}
+
+int isBoardFull(int grid[][SIZE]) {
+    int row, col;
+    for (row = 0; row < SIZE; row++) {
+        for (col = 0; col < SIZE; col++) {
+            if (grid[row][col] == 0) {
+                return 0; // There are still empty cells
             }
-			
-			printf("\nEnter your name: ");scanf("%s", &p1.player);
-            system("cls");
-			fputs(enter,fscore);
-            fprintf(fscore,"%-20s%5d\n",p1.player,p1.lives);
-            fclose(fscore);
-            display();
-            system("pause");
-            system("cls");
-                                
-			break;
-			}
-		}
-	}
-    if(p1.lives==0){
-        printf("\n\n\t\t\tYOU LOST");
-        puts("");
-		system("pause");
-        system("cls");
-		}
+        }
+    }
+    return 1; // All cells are filled
+}
+
+int solveSudoku(int grid[][SIZE]) {
+    int row, col, num;
+    for (row = 0; row < SIZE; row++) {
+        for (col = 0; col < SIZE; col++) {
+            if (grid[row][col] == 0) {
+                for (num = 1; num <= SIZE; num++) {
+                    if (isValidMove(grid, row, col, num)) {
+                        grid[row][col] = num;
+                        if (solveSudoku(grid)) {
+                            return 1;
+                        }
+                        grid[row][col] = 0;
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+int nextround(int grid[][SIZE]) {
+    int row, col;
+    for (row = 0; row < SIZE; row++) {
+        for (col = 0; col < SIZE; col++) {
+            if (grid[row][col] == 0) {
+                return 1; // There are still empty cells
+            }
+        }
+    }
+    return 0; // All cells are filled
+}
+
+void check(int grid[][SIZE], int row, int col) {
+    int solution = 0;
+    int num;
+
+    printf("\nEnter the number: ");
+    scanf("%d", &num);
+
+    if (isValidMove(grid, row, col, num)) {
+        grid[row][col] = num;
+        printf("\nValid Move!\n");
+        printBoard(grid);
+        solution = solveSudoku(grid);
+        if (solution && !nextround(grid)) {
+            printf("\nCongratulations! You have solved the Sudoku!\n");
+        } else {
+            printf("\nContinue solving...\n");
+        }
+    } else {
+        printf("\nInvalid Move! Try again.\n");
+    }
+}
+
+void sudoku(int grid[][SIZE]) {
+    int row, col;
+    printf("\nStart solving the Sudoku!\n");
+    while (nextround(grid)) {
+        printf("\nEnter the row (0-8) and column (0-8) to check: ");
+        scanf("%d %d", &row, &col);
+        if (row >= 0 && row < SIZE && col >= 0 && col < SIZE) {
+            if (grid[row][col] == 0) {
+                check(grid, row, col);
+            } else {
+                printf("\nThe cell is already filled. Try another one.\n");
+            }
+        } else {
+            printf("\nInvalid input! Try again.\n");
+        }
+    }
+}
+
+void saveScore(struct score player) {
+    FILE *file = fopen("highscores.txt", "a");
+    if (file != NULL) {
+        fprintf(file, "%s %d\n", player.player, player.lives);
+        fclose(file);
+    }
+}
+
+void displayHighscores() {
+    printf("\n    High Scores\n");
+    printf("  -----------------\n");
+    FILE *file = fopen("highscores.txt", "r");
+    if (file != NULL) {
+        char name[20];
+        int lives;
+        while (fscanf(file, "%s %d", name, &lives) != EOF) {
+            printf("  %-15s %d\n", name, lives);
+        }
+        fclose(file);
+    }
+    printf("  -----------------\n");
+}
+
+void solveSudokuPuzzle(int grid[][SIZE]) {
+    if (solveSudoku(grid)) {
+        printf("\nSolution found!\n");
+        printBoard(grid);
+    } else {
+        printf("\nNo solution exists for the given Sudoku puzzle.\n");
+    }
+}
+
+void copyGrid(int grid[][SIZE], int copyGrid[][SIZE]) {
+    int row, col;
+    for (row = 0; row < SIZE; row++) {
+        for (col = 0; col < SIZE; col++) {
+            copyGrid[row][col] = grid[row][col];
+        }
+    }
+}
+
+int main() {
+    int choice, level;
+    struct score player;
+
+    printf("\nWelcome to Sudoku Game!\n");
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Play Game\n");
+        printf("2. Display High Scores\n");
+        printf("3. Solve Sudoku Puzzle\n");
+        printf("4. Exit\n");
+        printf("Enter your choice (1-4): ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("\nSelect Difficulty Level:\n");
+                printf("1. Easy\n");
+                printf("2. Medium\n");
+                printf("3. Hard\n");
+                printf("Enter your choice (1-3): ");
+                scanf("%d", &level);
+                playGame(level);
+                break;
+            case 2:
+                displayHighscores();
+                break;
+            case 3: {
+                int grid[SIZE][SIZE];
+                printf("\nEnter the Sudoku puzzle (use 0 for empty cells):\n");
+                for (int i = 0; i < SIZE; i++) {
+                    for (int j = 0; j < SIZE; j++) {
+                        printf("Enter the number for row %d, column %d: ", i, j);
+                        scanf("%d", &grid[i][j]);
+                    }
+                }
+                printf("\nSolving Sudoku puzzle...\n");
+                solveSudokuPuzzle(grid);
+                break;
+            }
+            case 4:
+                printf("\nThank you for playing Sudoku! Goodbye!\n");
+                break;
+            default:
+                printf("\nInvalid choice! Try again.\n");
+        }
+
+    } while (choice != 4);
+
+    return 0;
 }
